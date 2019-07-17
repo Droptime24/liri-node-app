a9cf6b40d43e8251b0fb95f4c9d2c34db4d04e93
David Luce <david.luce@outlook.com>
2019-07-16 23:41:23 -0600

    added switch case to liri.js

--- liri.js
+++ liri.js
@@ -1,2 +1,24 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify = require("node-spotify-api")
+var spotify = new Spotify(keys.spotify);
+
+var operation = process.argv[2];
+
+function handler(operation){
+    switch (operation) {
+        case 'concert-this':
+            console.log('concert-this');
+            break;
+        case 'spotify-this-song':
+            console.log('spotify-this-song')
+        case 'movie-this':
+            console.log('movie-this');
+            // expected output: "Mangoes and papayas are $2.79 a pound."
+            break;
+        default:
+            console.log('do-what-it-says');
+    }
+}
+
+handler(operation);
