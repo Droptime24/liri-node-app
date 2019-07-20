eeab0a81f5a04e93c04a336bb69b04e53240b679
David Luce <david.luce@outlook.com>
2019-07-20 00:54:12 -0600

    updated switch statment

--- liri.js
+++ liri.js
@@ -7,49 +7,78 @@ var spotify = new Spotify(keys.spotify);
 var fs = require('fs')
 
 
-var liri = process.argv[2];
+var Liri = function (myCase, myFunction) {
 
-// var text = process.argv[3];
-switch (liri) {
+    switch (myCase) {
 
-    case 'concert-this':
-        artist();
-        break;
+        case 'concert-this':
+            findConcert(myFunction);
+            break;
 
-    case 'spotify-this':
-        song();
-        break;
+        case 'spotify-this':
+            searchSong(myFunction.Spotify);
+            break;
 
-    case 'movie-this':
-        movie();
-        break;
+        case 'movie-this':
+            findMovie(myFunction);
+            break;
 
-    default: 'do-what-it-says';
-        console.log('do-what-it-says');
-}
+        case 'do-what-it-says':
+            justDoIt()
+            console.log('Does not compute!! Liri shutting down.');
+    }
+};
+
 
 // bands in town
-
-function artist(){
+function findConcert() {
 
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function (response) {
         for (i = 0; i < response.lenght; i++) {
-            return artist;
+            return venue.name;
         }
     });
-}
+};
 
 // spotify
+function searchSong() {
+    spotify.search(
+        {
+            type: 'string',
+            query: 'All the Small Things'
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+};
+searchSong()
 
-spotify.search(
-    {
-        type: 'search',
-        query: 'All the Small Things'
+function findMovie(){
+axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+    function (response) {
+        console.log("The movie's rating is: " + response.data.imdbRating);
     })
-
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (err) {
-        console.log(err); 
+    .catch(function (error) {
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log("---------------Data---------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+        }
+        console.log(error.config);
     });
+};
