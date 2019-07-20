0c6d0ba3dd8d51858e38a4931618907b85e2cf12
David Luce <david.luce@outlook.com>
2019-07-19 23:44:32 -0600

    added in axios call, spotify call and functions

--- liri.js
+++ liri.js
@@ -1,30 +1,55 @@
-require("dotenv").config();
+require('dotenv').config();
 
-var axios = require("axios");
-var keys = require("./keys.js");
-var Spotify = require("node-spotify-api")
+var axios = require('axios')
+var keys = require('./keys.js');
+var Spotify = require('node-spotify-api')
 var spotify = new Spotify(keys.spotify);
+var fs = require('fs')
 
-var operation = process.argv[2];
 
-function handler(operation){
-    switch (operation) {
-        case 'concert-this':
-            console.log('concert-this');
-            break;
-        case 'spotify-this':
-            songs()
-        case 'movie-this':
-            console.log('movie-this');
-            // expected output: "Mangoes and papayas are $2.79 a pound."
-            break;
-        default:
-            console.log('do-what-it-says');
-    }
+var liri = process.argv[2];
+
+// var text = process.argv[3];
+switch (liri) {
+
+    case 'concert-this':
+        artist();
+        break;
+
+    case 'spotify-this':
+        song();
+        break;
+
+    case 'movie-this':
+        movie();
+        break;
+
+    default: 'do-what-it-says';
+        console.log('do-what-it-says');
 }
 
-handler(operation);
+// bands in town
 
-axios.get();
+function artist(){
 
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function (response) {
+        for (i = 0; i < response.lenght; i++) {
+            return artist;
+        }
+    });
+}
 
+// spotify
+
+spotify.search(
+    {
+        type: 'search',
+        query: 'All the Small Things'
+    })
+
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (err) {
+        console.log(err); 
+    });
